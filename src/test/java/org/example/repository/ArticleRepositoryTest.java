c21f69b9030f8e7fe18b320876754b80bb3b54da
박승찬 <seungchan141414@gmail.com>
2024-02-04 01:47:22 +0900

    Feat : Add Test

--- src/test/java/org/example/repository/ArticleRepositoryTest.java
+++ src/test/java/org/example/repository/ArticleRepositoryTest.java
@@ -62,4 +62,29 @@ class ArticleRepositoryTest {
         assertThat(article4.getDeclarationCount()).isEqualTo(0);
 
     }
+
+    @Test
+    void Article_좋아요_업데이트_테스트() {
+        String title = "test title";
+        String content = "test conten";
+        Article article = this.articleRepository.save(Article.builder()
+                .title(title)
+                .content(content)
+                .build());
+
+        Article article0 = this.articleRepository.findById(article.getArticleId()).get();
+
+        article0.addLike();
+        Article article1 = this.articleRepository.save(article0);
+        assertThat(article1.getLikeCount()).isEqualTo(1);
+
+        Article article2 = this.articleRepository.save(article1);
+        article2.subtractLike();
+        Article article3 = this.articleRepository.save(article2);
+        assertThat(article3.getLikeCount()).isEqualTo(0);
+
+        article3.subtractLike();
+        Article article4 = this.articleRepository.save(article3);
+        assertThat(article4.getLikeCount()).isEqualTo(0);
+    }
 }
