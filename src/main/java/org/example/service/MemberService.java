5d5e2148bef531c7aba24c7f60f0f9d496437788
박승찬 <seungchan141414@gmail.com>
2024-02-14 16:50:51 +0900

    Remove : MemberAdmin, MemberInformation 삭제

--- src/main/java/org/example/service/MemberService.java
+++ src/main/java/org/example/service/MemberService.java
@@ -2,8 +2,6 @@ package org.example.service;
 
 import org.example.constant.MBTI;
 import org.example.entity.Member;
-import org.example.entity.MemberAdmin;
-import org.example.entity.MemberInformation;
 import org.example.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,21 +21,9 @@ public class MemberService {
         return memberRepository.findById(id).get();
     }
 
-    /**
-     * 지연 로딩LAZY: 연관된 엔티티를 프정록시로 조회한다. 프록시를 실제 사용할 때 초기화하면서 데이터베이스를 조회한다.
-     *
-     * @param id
-     * @return
-     */
-    public MBTI getMbti(Long id) {
-        Member member = memberRepository.findById(id).get();
-        MemberInformation memberInformation = member.getMemberInformation();
-        return memberInformation.getMbti();
-    }
 
     public String getEmail(Long id) {
         Member member = memberRepository.findById(id).get();
-        MemberAdmin memberAdmin = member.getMemberAdmin();
-        return memberAdmin.getEmail();
+        return member.getEmail();
     }
 }
--- src/test/java/org/example/testutill/TestSetUp.java
+++ src/test/java/org/example/testutill/TestSetUp.java
@@ -3,59 +3,58 @@ package org.example.testutill;
 import org.example.constant.MBTI;
 import org.example.constant.MEMBER_GRADE;
 import org.example.entity.Member;
-import org.example.entity.MemberAdmin;
-import org.example.entity.MemberInformation;
 import org.example.myutill.CustomLocalDate;
-import org.example.repository.MemberInformationRepository;
-import org.example.repository.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.stereotype.Component;
 
 public class TestSetUp {
 
-    public static MemberInformation getMemberInformation() {
-        String gpa = "4.5";
-        String hobby = "달리기";
-        String photo = "사진";
-        String specialtySkill = "100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 ";
-        String advantage = "100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 ";
-        String disadvantage = "100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 ";
-        String introduction = "200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 ";
-        MBTI mbti = MBTI.ENTP;
-
-        return MemberInformation.builder()
-                .gpa(gpa)
-                .specialtySkill(specialtySkill)
-                .hobby(hobby)
-                .mbti(mbti)
-                .advantage(advantage)
-                .disadvantage(disadvantage)
-                .introduction(introduction)
-                .photo(photo)
-                .build();
-    }
-
-    public static Member getMember() {
+    //    public static MemberInformation getMemberInformation() {
+//        String gpa = "4.5";
+//        String hobby = "달리기";
+//        String photo = "사진";
+//        String specialtySkill = "100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 ";
+//        String advantage = "100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 ";
+//        String disadvantage = "100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 100자테스트 ";
+//        String introduction = "200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 200자테스트 ";
+//        MBTI mbti = MBTI.ENTP;
+//
+//        return MemberInformation.builder()
+//                .gpa(gpa)
+//                .specialtySkill(specialtySkill)
+//                .hobby(hobby)
+//                .mbti(mbti)
+//                .advantage(advantage)
+//                .disadvantage(disadvantage)
+//                .introduction(introduction)
+//                .photo(photo)
+//                .build();
+//    }
+//
+    public static Member getRepresentativMember() {
+        String FIRST_NAME = "박";
+        String LAST_NAME = "승찬";
+        String NICK_NAME = "박";
+        String EMAIL = "test1234@test.com";
+        String MAJOR = "컴퓨터공학과";
+        String STUDENT_ID = "20240101";
         return Member.builder()
-                .firstName("박")
-                .lastName("승찬")
-                .phoneNumber("010-0000-0000")
-                .major("컴퓨터공학과")
-                .birthDate(CustomLocalDate.parse2BirthDateFromString("1996-04-15"))
-                .studentId("20249999")
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .nickName(NICK_NAME)
+                .email(EMAIL)
+                .major(MAJOR)
+                .studentId(STUDENT_ID)
                 .build();
     }
-
-    public static MemberAdmin getMemberAdmin() {
-        String email = "test@test.com";
-        String password = "1234";
-        MEMBER_GRADE memberGrade = MEMBER_GRADE.MEMBER;
-
-        return MemberAdmin.builder()
-                .email(email)
-                .password(password)
-                .grade(memberGrade)
-                .build();
-    }
+//
+//    public static MemberAdmin getMemberAdmin() {
+//        String email = "test@test.com";
+//        String password = "1234";
+//        MEMBER_GRADE memberGrade = MEMBER_GRADE.MEMBER;
+//
+//        return MemberAdmin.builder()
+//                .email(email)
+//                .password(password)
+//                .grade(memberGrade)
+//                .build();
+//    }
 }
