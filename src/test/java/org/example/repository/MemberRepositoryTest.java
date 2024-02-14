3940535f8d93182a491b6087055a9a9829630b4f
박승찬 <seungchan141414@gmail.com>
2024-02-14 16:52:01 +0900

    Feat : Member entity의 변경에 따른 테스트

--- src/test/java/org/example/repository/MemberRepositoryTest.java
+++ src/test/java/org/example/repository/MemberRepositoryTest.java
@@ -2,8 +2,6 @@ package org.example.repository;
 
 import org.example.constant.MBTI;
 import org.example.entity.Member;
-import org.example.entity.MemberAdmin;
-import org.example.entity.MemberInformation;
 import org.example.service.MemberService;
 import org.example.testutill.TestSetUp;
 import org.junit.jupiter.api.BeforeEach;
@@ -22,74 +20,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MemberRepositoryTest {
     private final MemberService memberService;
     private final MemberRepository memberRepository;
-    private final MemberInformationRepository memberInformationRepository;
-    private final MemberAdminRepository memberAdminRepository;
 
 
     @Autowired
-    MemberRepositoryTest(MemberService memberService, MemberRepository memberRepository, MemberInformationRepository memberInformationRepository, MemberAdminRepository memberAdminRepository) {
+    MemberRepositoryTest(MemberService memberService, MemberRepository memberRepository) {
         this.memberService = memberService;
         this.memberRepository = memberRepository;
-        this.memberInformationRepository = memberInformationRepository;
-        this.memberAdminRepository = memberAdminRepository;
     }
 
     @BeforeEach
     void setUp() {
         memberRepository.deleteAll();
-        memberInformationRepository.deleteAll();
-        memberAdminRepository.deleteAll();
     }
 
     @Test
     void Member_저장_테스트() {
-        // 현재 날짜 가져오기
-        LocalDate currentDate = LocalDate.now();
+        final Member representativMember = TestSetUp.getRepresentativMember();
 
-        // 원하는 형식의 문자열로 포맷팅하기
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy");
-        String formattedDate = currentDate.format(formatter);
+        memberRepository.save(representativMember);
 
-        // 1. 매핑 준비
-        Member member = TestSetUp.getMember();
-        MemberInformation memberInformation = TestSetUp.getMemberInformation();
-        MemberAdmin memberAdmin = TestSetUp.getMemberAdmin();
+        Member targetMember = memberRepository.findById(representativMember.getMemberId()).get();
 
-        // 2. 매핑 member, memberInformation set
-        member.setMemberInformation(memberInformation);
-        member.setMemberAdmin(memberAdmin);
-
-        // 등록
-        Member member1 = memberRepository.save(member);
-
-        Member target = memberRepository.findById(member1.getMemberId()).get();
-        assertThat(target.getFirstName()).isEqualTo("박");
-        assertThat(target.getLastName()).isEqualTo("승찬");
-        assertThat(target.getPhoneNumber()).isEqualTo("010-0000-0000");
-        assertThat(target.getRegistration()).isEqualTo(formattedDate);
-
-        MBTI mbti = memberService.getMbti(target.getMemberId());
-        System.out.println("mbti = " + mbti);
-
-        String email = memberService.getEmail(target.getMemberId());
-        System.out.println("email = " + email);
+        assertThat(targetMember.getFirstName()).isEqualTo(targetMember.getFirstName());
+        assertThat(targetMember.getEmail()).isEqualTo(targetMember.getEmail());
+        assertThat(targetMember.getStudentId()).isEqualTo(targetMember.getStudentId());
     }
 
     @Test
     void Member_update_테스트() {
         // 1. 매핑 준비
-        Member member = TestSetUp.getMember();
-        MemberInformation memberInformation = TestSetUp.getMemberInformation();
-        MemberAdmin memberAdmin = TestSetUp.getMemberAdmin();
 
-        // 2. 매핑 member, memberInformation set
-        member.setMemberInformation(memberInformation);
-        member.setMemberAdmin(memberAdmin);
-
-        // 등록
-        Member testMember = memberRepository.save(member);
-//        memberInformationRepository.delete(testMember.getMemberInformation());
-        memberRepository.delete(testMember);
-//        memberAdminRepository.delete(testMember.getMemberAdmin());
     }
 }
