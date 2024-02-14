3adadabd3a8714e171bacc16f5be8383c9661217
박승찬 <seungchan141414@gmail.com>
2024-02-14 16:52:13 +0900

    Feat : Test entity에 따른 변경

--- src/main/java/org/example/repository/TestEntityRepository.java
+++ src/main/java/org/example/repository/TestEntityRepository.java
@@ -1,6 +1,5 @@
 package org.example.repository;
 
-import org.example.entity.MemberAdmin;
 import org.example.entity.TestEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
